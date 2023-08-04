fec0abb763c30e2472a9d9cfd32593e362e75d2d
Luis Gustavo De Jesus Hunzecher <Luis Gustavo de Jesus Hunzecher>
2023-08-04 09:01:41 -0300

    Atualização do IndexedDB

--- js/indexedDB.js
+++ js/indexedDB.js
@@ -14,7 +14,7 @@
                     if(!opcoes.hasOwnProperty("bdVersao")){
                         reject(console.error("O objeto Opções não contem a Versão do Banco de Dados, verifique e tente novamente!"));
                     }
-    
+
                     if(!opcoes.hasOwnProperty("bdTabelas")){
                         reject(console.error("O objeto Opções não contem a(s) Tabela(s) do Banco de Dados, verifique e tente novamente!"));
                     } else {
@@ -23,23 +23,23 @@
                         } else {
                             for(var i in opcoes.bdTabelas){
                                 var tabela = opcoes.bdTabelas[i];
-    
+
                                 if(!tabela.hasOwnProperty("nome")){
                                     reject(console.error("A Tabela não possui um nome válido, verifique e tente novamente!"));
                                 }
-    
+
                                 if(!tabela.hasOwnProperty("parametros")){
                                     reject(console.error("A Tabela não possui nenhum parametro válido, verifique e tente novamente!"));
                                 } else {
                                     if(!tabela.parametros.hasOwnProperty("keyPath")){
                                         reject(console.error("A Tabela não possui um keyPath válido, verifique e tente novamente!"));
                                     }
-    
+
                                     if(!tabela.parametros.hasOwnProperty("autoIncrement")){
                                         reject(console.error("A Tabela não possui um autoIncrement válido, verifique e tente novamente!"));
                                     }
                                 }
-    
+
                                 if(!tabela.hasOwnProperty("index")){
                                     reject(console.error("A Tabela não possui um Index Válido, verifique e tente novamente!"));
                                 } else {
@@ -48,46 +48,46 @@
                                     } else {
                                         for(var j in tabela.index){
                                             var index = tabela.index[j];
-    
+
                                             if(!index.hasOwnProperty("nome")){
                                                 reject(console.error("O Index precisa ter um nome válido, verifique e tente novamente!"));
                                             }
-    
+
                                             if(!index.hasOwnProperty("campo")){
                                                 reject(console.error("O Index precisa ter um campo válido, verifique e tente novamente!"));
                                             }
-    
+
                                             if(!index.hasOwnProperty("opcoes")){
                                                 reject(console.error("O Index precisa ter um opções válidas, verifique e tente novamente!"));
                                             }
                                         }
                                     }
                                 }
-    
+
                             }
                         }
                     }
-    
+
                 // Cria o Banco de Dados (IndexedDB)
-    
+
                     var bd = indexedDB.open(opcoes.bdNome, opcoes.bdVersao);
-    
+
                 // Cria os Estados da Conexão
 
                     bd.onsuccess = (evento)=>{
                         resolve(evento.target);
                     }
-    
+
                     bd.onerror = (evento)=>{
                         console.error("O Banco de Dados ["+opcoes.bdNome+"] retornou o seguinte erro: " + evento.target.errorCode);
                     }
-    
+
                     bd.onupgradeneeded = (event)=>{
                         var request = event.target.result;
                         for(var i in opcoes.bdTabelas){
                             var tabela = opcoes.bdTabelas[i];
                             var objetoTabela = request.createObjectStore(tabela.nome, tabela.parametros);
-    
+
                             for(var j in tabela.index){
                                 var index = tabela.index[j];
                                 objetoTabela.createIndex(index.nome, index.campo, index.opcoes);
@@ -106,11 +106,11 @@
                 if(typeof tabela != "string"){
                     reject(console.error("O Nome da Tabela é inválido ou não existente"));
                 }
-    
+
                 if(typeof bd != "object"){
                     reject(console.error("O Objeto bd é inválido ou não existente"));
                 }
-    
+
                 if(typeof dados != "object"){
                     reject(console.error("Os Dados são inválidos ou não existentes"));
                 } else {
@@ -118,7 +118,7 @@
                         reject(console.error("Insira pelomenos 1 dado"));
                     }
                 }
-    
+
                 var request = bd.result.transaction(tabela, "readwrite").objectStore(tabela);
                 var promessas = [];
                 
@@ -227,7 +227,7 @@
                 }
 
                 var objBDStore = bd.result.transaction(tabela).objectStore(tabela);
-                var objBDCursor = objBDStore.index(index).openCursor(IDBKeyRange.bound(valor, valor+"\uffff"), ordenacao == "asc" ? "next" : "prev");
+                var objBDCursor = objBDStore.index(index).openCursor(null, ordenacao == "asc" ? "next" : "prev");
                 var data = [];
 
                 if(Object.keys(paginacao) <= 0){
@@ -246,7 +246,9 @@
                         var cursor = evento.target.result;
                         if(cursor){
                             if(count >= (paginacao.paginaAtual-1) * paginacao.registrosPorPagina && count < paginacao.paginaAtual * paginacao.registrosPorPagina){
-                                data.push(cursor.value);
+                                if(cursor.value[index].includes(valor)){
+                                    data.push(cursor.value);
+                                }
                             }
                             cursor.continue();
                             count++;
@@ -261,7 +263,7 @@
 
     // Função para Pesquisa no Banco de Dados por Código
         
-        function TotalRegistrosTabela(bd, tabela){
+        function TotalRegistrosTabela(bd, tabela, pesquisa = {valor: "", index: ""}){
             return new Promise((resolve, reject)=>{
                 if(typeof bd != "object"){
                     reject(console.error("O Objeto bd é inválido ou não existente"));
@@ -271,11 +273,34 @@
                     reject(console.error("O Nome da Tabela é inválido ou não existente"));
                 }
                 
+                if(typeof pesquisa != "object"){
+                    reject(console.error("O Objeto pesquisa é inválido ou não existente"));
+                }
+                
                 var objBDStore = bd.result.transaction(tabela).objectStore(tabela);
-                var objBDCursor = objBDStore.getAll();
+                
+                if(pesquisa.valor != ""){
+                    //var objBDCursor = objBDStore.index(pesquisa.index).openCursor(IDBKeyRange.bound(pesquisa.valor, pesquisa.valor+"\uffff"), pesquisa.ordenacao == "asc" ? "next" : "prev");
+                    var objBDCursor = objBDStore.index(pesquisa.index).openCursor(null, pesquisa.ordenacao == "asc" ? "next" : "prev");
+                    var data = [];
+                    
+                    objBDCursor.onsuccess = (evento)=>{
+                        var cursor = evento.target.result;
+                        if(cursor){
+                            if(cursor.value[pesquisa.index].includes(pesquisa.valor)){
+                                data.push(cursor.value);
+                                cursor.continue();
+                            }
+                        } else {
+                            resolve(data.length);
+                        }
+                    }
+                } else {
+                    var objBDCursor = objBDStore.getAll();
 
-                objBDCursor.onsuccess = (evento)=>{
-                    resolve(evento.target.result.length);
+                    objBDCursor.onsuccess = (evento)=>{
+                        resolve(evento.target.result.length);
+                    }
                 }
             })
         }
@@ -287,48 +312,48 @@
                 if(typeof bd != "object"){
                     reject(console.error("O Objeto bd é inválido ou não existente"));
                 }
-    
+
                 if(typeof tabela != "string"){
                     reject(console.error("O Nome da Tabela é inválido ou não existente"));
                 }
-    
+
                 if(typeof codigo != "number"){
                     reject(console.error("O Codigo do Registro da Tabela é inválido ou não existente"));
                 }
-    
+
                 if(typeof dados != "object"){
                     reject(console.error("Os Dados da Tabela são inválidos ou não existentes"));
                 }
-    
+
                 var objetoTabela = bd.result.transaction([tabela], "readwrite").objectStore(tabela);
                 var requisicao = objetoTabela.get(codigo);
-    
+
                 requisicao.onerror = (evento)=>{
                     reject(console.error("Houve um erro na transação: "+evento.target.errorCode));
                 };
-    
+
                 requisicao.onsuccess = (evento)=>{
                     var registro = requisicao.result;
                     var campos = Object.keys(dados);
-    
+
                     for(var i in campos){
                         var campo = campos[i];
                         registro[campo] = dados[campo];
                     }
-    
+
                     var objetoRegistro = objetoTabela.put(registro);
-    
+
                     objetoRegistro.onerror = function(evento) {
                         reject(console.error("Houve um erro na atualização do registro: "+evento.target.errorCode));
                     };
-    
+
                     objetoRegistro.onsuccess = function(evento) {
                         resolve(console.log("Registro Atualizado com Sucesso"));
                     };
                 }
             })
         }
-    
+
     // Função para Excluir o Registro no Banco de Dados
 
         function excluiRegistroBD(bd, tabela, codigo){
@@ -336,27 +361,41 @@
                 if(typeof bd != "object"){
                     reject(console.error("O Objeto bd é inválido ou não existente"));
                 }
-    
+
                 if(typeof tabela != "string"){
                     reject(console.error("O Nome da Tabela é inválido ou não existente"));
                 }
-    
+
                 if(typeof codigo != "number"){
                     reject(console.error("O Codigo do Registro da Tabela é inválido ou não existente"));
                 }
-    
+
                 var requisicao = bd.result.transaction([tabela], "readwrite").objectStore(tabela).delete(codigo);
-    
+
                 requisicao.onerror = (evento)=>{
                     reject(console.error("Houve um erro na exclusão: "+evento.target.errorCode));
                 };
-    
+
                 requisicao.onsuccess = (evento)=>{
                     resolve(console.log("Registro Excluído com Sucesso"));
                 }
             })
         }
-    
+        
+    // Limpa Tabela
+
+        function limpaTabelaBD(bd, tabela){
+            if(typeof bd != "object"){
+                reject(console.error("O Objeto bd é inválido ou não existente"));
+            }
+
+            if(typeof tabela != "string"){
+                reject(console.error("O Nome da Tabela é inválido ou não existente"));
+            }
+            
+            bd.result.transaction(tabela, "readwrite").objectStore(tabela).clear();
+        }
+
     // Função para Fechar o Banco de Dados
 
         function fechaBD(bd){
